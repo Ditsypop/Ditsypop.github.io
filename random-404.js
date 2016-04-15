fb0483d459e262ffca2a1b665cf9a4256aecee6a
Ditsypop <Ditsypop@gmail.com>
2016-04-15 15:17:50 -0500

    Fixed/modified paths, still can't decide on fs
    fuck me right

--- random-404.js
+++ random-404.js
@@ -1,10 +1,11 @@
+//script courtesy of https://catbox.moe/pic.js
 var random_images_array = 
 [
 "404-1.png", 
 "404-2.png", 
 ];
 
-path = 'css/images/'; 
+path = '/images/404/'; 
 var num = Math.floor( Math.random() * random_images_array.length );
 var img = random_images_array[ num ];
 var imgStr = '<img src="' + path + img + '">';
